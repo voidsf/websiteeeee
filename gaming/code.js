b953b2460ec5fc63d2e99657b92543891e496c55
voidsf <samanthafisher05@hotmail.com>
2022-06-20 15:42:33 +0100

    angry comments

--- gaming/code.js
+++ gaming/code.js
@@ -1,3 +1,6 @@
 var canvas = document.getElementById("drawcanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#000000";
+
+//none of this shit works! how do i do javascript
+//got damn!!!
