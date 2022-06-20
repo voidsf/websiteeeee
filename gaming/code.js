cdcbce58073daebba6c53ac2eb97454d4f532845
voidsf <samanthafisher05@hotmail.com>
2022-06-20 18:40:26 +0100

    changes

--- gaming/code.js
+++ gaming/code.js
@@ -1,5 +1,9 @@
-function draw(){
+var canvas = document.getElementById("drawcanvas");
+var ctx = canvas.getContext("2d");
 
+function draw(){
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0,0,150,75);
 }
 
 draw();
